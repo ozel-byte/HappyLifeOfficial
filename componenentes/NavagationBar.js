914e16ac2651025d039e7b75bae2919c5b764a4d
ozel <ozelcode@gmail.com>
2021-03-13 14:01:33 -0600

    se Creo componente

---
+++ componenentes/NavagationBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Footer from "./Footer";
+
+
+class NavagationBar extends React.Component{
+    render() {
+        return (
+            <>
+                <header className="header">
+                    <div className="logo-container animate__animated animate__bounce">
+                        <div className="animate__animated animate__backInLeft"> <img src="../imgs/icon.png" alt="" width="60px" height="60px"/></div>
+                        <h2>HappyLife</h2>
+                    </div>
+                    <nav>
+                        <ul className="nav-items">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Acerca de Nosotros</a></li>
+                            <li><a href="#">Equipo</a></li>
+                            <li><a href="#">Servicio</a></li>
+                        </ul>
+                    </nav>
+                    <div className="contenedor-botones">
+                        <button className="boton-Iniciar-session">Iniciar Sesion</button>
+                        <button className="boton-Iniciar-session">Registrarse</button>
+                    </div>
+                </header>
+            </>
+        )
+    }
+}
+
+export default NavagationBar;
