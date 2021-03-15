6fce8fb077526ee4c51a0a83856ab463208b4287
Kevin O <Kevinnuricumbo@gmail.com>
2021-03-14 19:21:31 -0600

    NavBarDashboard

--- app/dashboard.js
+++ app/dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NavBarDashboard from "./NavBarDashboard";
 
 
 class Dashboard extends React.Component{
@@ -11,32 +12,7 @@ class Dashboard extends React.Component{
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <div className="container-fluid">
-                        <div>
-
-                            <a className="navbar-brand" href="#"><img src="../imgs/IMG_20210201_170350.png" alt="" width="40px" height="40px"/>HappyLife</a>
-                        </div>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav nav-customer">
-                                <li className="nav-item">
-                                    <a className="nav-link" aria-current="page" href="#">Rincon de la felicidad</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="/BaulRecuerdos">Baul de los Recuerdos</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Diario Emocional</a>
-                                </li>
-
-                            </ul>
-
-                        </div>
-                        <div className="d-flex justify-content-end align-items-center divButton">
-                                <h5>Ozel^</h5>
-                        </div>
-                    </div>
-                </nav>
+                <NavBarDashboard/>
                <div className="contenedor-publicacion">
                   <h1>Rincon de la Felicidad</h1>
                    <label className="form-label text-muted">Anote todas las cosas que lo hicieron feliz</label>
